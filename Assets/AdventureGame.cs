fb2f66a34dd729298c6fc837944b3f731078319e
Laurie Trichet <laurie.trichet@gmail.com>
2019-02-03 12:48:50 +1100

    add UI and states

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TMPro.TMP_Text title;
+    [SerializeField] TMPro.TMP_Text description;
+    [SerializeField] State startingState;
+
+    State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        description.text = startingState.Story;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.NextStates;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (nextStates.Length > 1)
+            {
+                state = nextStates[1];
+            }
+           
+        }
+
+        description.text = state.Story;
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+
+    [TextArea(10,14)][SerializeField] string storyText;
+
+    [SerializeField] State[] nextStates;
+
+    public State[] NextStates
+    {
+        get
+        {
+            return nextStates;
+        }
+    }
+
+    public string Story {
+        get
+        {
+            return storyText;
+        }  
+    }
+}
